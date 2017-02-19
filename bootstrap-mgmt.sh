cc9af243d9f163c5c131772f2b2dc951a4fa33de
Jacob Stoebel <jstoebel@gmail.com>
2017-02-19 18:49:35 -0500

    update to bootstrap script and roles

--- bootstrap-mgmt.sh
+++ bootstrap-mgmt.sh
@@ -6,6 +6,10 @@ apt-add-repository -y ppa:ansible/ansible
 apt-get update
 apt-get -y install ansible
 
+# copy examples into /home/vagrant (from inside the mgmt node)
+cp -a /vagrant/examples/* /home/vagrant
+chown -R vagrant:vagrant /home/vagrant
+
 # configure hosts file for our internal network defined by Vagrantfile
 cat >> /etc/hosts <<EOL
 
